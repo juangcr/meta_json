7694231a540003cce9a0b07ee78c09bee0584e75
Juan G. Cortés <juang.cortes@outlook.com>
2024-04-21 23:12:46 -0600

    Integration test format change.

--- src/tests/integration/test_meta_json.py
+++ src/tests/integration/test_meta_json.py
@@ -22,28 +22,28 @@ def test_integration():
 
         print(f"\n Object definition lapse : {t2 - t1} seconds.\n")
 
-        t1 = default_timer()
         with open(f"{json_path}/sample_types.json", "w",
                   encoding="utf-8") as j:
-            j.write(f"// Lapse : {t2 - t1} seconds.\n")
+            t1 = default_timer()
             j.write(str(meta.types()))
-        t2 = default_timer()
+            t2 = default_timer()
+            j.write(f"\n// Lapse : {t2 - t1} seconds.\n")
         print(f"\nMeta types lapse : {t2 - t1} seconds.\n")
 
-        t1 = default_timer()
         with open(f"{json_path}/sample_attrs.json", "w",
                   encoding="utf-8") as j:
-            j.write(f"// Lapse : {t2 - t1} seconds.\n")
+            t1 = default_timer()
             j.write(str(meta.attributes()))
-        t2 = default_timer()
+            t2 = default_timer()
+            j.write(f"\n// Lapse : {t2 - t1} seconds.\n")
         print(f"\nMeta attributes lapse : {t2 - t1} seconds.\n")
 
-        t1 = default_timer()
         with open(f"{json_path}/sample_layers.json", "w",
                   encoding="utf-8") as j:
-            j.write(f"// Lapse : {t2 - t1} seconds.\n")
+            t1 = default_timer()
             j.write(str(meta.layers()))
-        t2 = default_timer()
+            t2 = default_timer()
+            j.write(f"\n// Lapse : {t2 - t1} seconds.\n")
         print(f"\nMeta layers lapse : {t2 - t1} seconds.\n")
 
         assert True
