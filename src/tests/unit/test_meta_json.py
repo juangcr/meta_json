10e882c6f78d94781ffd6ba793f06b517f7fd9b8
Juan G. Cortés <juang.cortes@outlook.com>
2025-11-22 18:07:33 -0600

    Testing dependencies fixed

--- src/tests/unit/test_meta_json.py
+++ src/tests/unit/test_meta_json.py
@@ -1,10 +1,8 @@
-import pytest
 from meta_json import MetaJson
 
 
 def test_meta_json_empty():
-    """Read the TESTING.md document for more information.
-    """
+    """Read the TESTING.md document for more information."""
     meta = MetaJson(response={})
     assert meta.types() == {}
     assert meta.attributes() == [[], []]
--- src/tests/unit/test_meta_json_parser.py
+++ src/tests/unit/test_meta_json_parser.py
@@ -1,4 +1,3 @@
-import pytest
 from meta_json.attribute_parser import AttributesParser
 from meta_json.layer_parser import LayersParser
 from meta_json.type_parser import TypesParser
@@ -7,80 +6,65 @@ from meta_json.type_parser import TypesParser
 """Read the TESTING.md document for more information."""
 
 EXAMPLE_1 = [
-	"item_1",
+    "item_1",
     "item_2",
-	["item_3", "item_4", "item_5"],
+    ["item_3", "item_4", "item_5"],
     [["item_6"]],
-	[[], [], []],
-	[], 
-    ]
+    [[], [], []],
+    [],
+]
 
 
 EXAMPLE_2 = {
-	"name": "John Doe",
-	"height": 1.73,
-	"mass": 77,
-	"hair_color": "blond",
-	"skin_color": "fair",
-	"eye_color": "blue",
-	"birth_year": "1970",
-	"gender": "male",
-	"species": ["Human"],
-	"created": "2023-03-09T13:50:41.674000Z",
-	"edited": "2023-03-20T21:17:53.791000Z",
-	"url": "https://www.url.dev"
+    "name": "John Doe",
+    "height": 1.73,
+    "mass": 77,
+    "hair_color": "blond",
+    "skin_color": "fair",
+    "eye_color": "blue",
+    "birth_year": "1970",
+    "gender": "male",
+    "species": ["Human"],
+    "created": "2023-03-09T13:50:41.674000Z",
+    "edited": "2023-03-20T21:17:53.791000Z",
+    "url": "https://www.url.dev",
 }
 
 
 EXAMPLE_3 = {
     "layer1_a": "text",
     "layer1_b": 3.14,
-    "layer1_c": {
-        "layer2_a": 42,
-        "layer2_b": "more text"
-        },
+    "layer1_c": {"layer2_a": 42, "layer2_b": "more text"},
     "layer1_d": ["bit", "more", "text"],
     "layer1_e": {
-        "layer2_c": [
-            {"layer3_a": "deep"},
-            {"layer3_b": "deeper"}
-            ],
-        "layer2_d": {
-            "layer3_c": {"layer4_a": "deepest"}
-        }
-    }
+        "layer2_c": [{"layer3_a": "deep"}, {"layer3_b": "deeper"}],
+        "layer2_d": {"layer3_c": {"layer4_a": "deepest"}},
+    },
 }
 
 
 def test_datetimes():
     data = [
-	    "2014-12-19T13:50:51.644000Z",
-	    "2014/12/20 21:17:56",
-	    "21-12-2014 22:54:00",
-	    "22/12/2014",
-	    "12-23-2014 12:34:56 EST",
-	    "12/24/2014",
-	    "https://api.dev/api/1/"
-        ]
+        "2014-12-19T13:50:51.644000Z",
+        "2014/12/20 21:17:56",
+        "21-12-2014 22:54:00",
+        "22/12/2014",
+        "12-23-2014 12:34:56 EST",
+        "12/24/2014",
+        "https://api.dev/api/1/",
+    ]
 
     meta = TypesParser()
     results = [meta._parse_datetimes(d) for d in data]
-    assert results == [True, True, True, True, True, True, False] 
+    assert results == [True, True, True, True, True, True, False]
 
 
 def test_flat_list():
-    output_data = [
-	    "item_1",
-	    "item_2",
-	    "item_3",
-	    "item_4",
-	    "item_5",
-	    "item_6"
-        ]
+    output_data = ["item_1", "item_2", "item_3", "item_4", "item_5", "item_6"]
 
     meta = AttributesParser()
     result = meta._hard_flatten(EXAMPLE_1)
-    assert result == output_data 
+    assert result == output_data
 
 
 def test_flat_empty_list():
@@ -91,20 +75,20 @@ def test_flat_empty_list():
 
 def test_partial_flat_list():
     output_data = [
-	    "item_1",
+        "item_1",
         "item_2",
-	    "item_3",
-	    "item_4",
-	    "item_5",
+        "item_3",
+        "item_4",
+        "item_5",
         ["item_6"],
         [],
-	    [],
-	    [], 
-        ]
+        [],
+        [],
+    ]
 
     meta = LayersParser()
     result = meta._soft_flatten(EXAMPLE_1)
-    assert result == output_data 
+    assert result == output_data
 
 
 def test_partial_flat_empty_list():
@@ -115,18 +99,18 @@ def test_partial_flat_empty_list():
 
 def test_types_one_layer():
     output_data = {
-		"name": "str",
-	    "height": "float",
-	    "mass": "int",
-	    "hair_color": "str",
-	    "skin_color": "str",
-	    "eye_color": "str",
-	    "birth_year": "str",
-	    "gender": "str",
-	    "species": ["str"],
-    	"created": "datetime",
-	    "edited": "datetime",
-    	"url": "str" 
+        "name": "str",
+        "height": "float",
+        "mass": "int",
+        "hair_color": "str",
+        "skin_color": "str",
+        "eye_color": "str",
+        "birth_year": "str",
+        "gender": "str",
+        "species": ["str"],
+        "created": "datetime",
+        "edited": "datetime",
+        "url": "str",
     }
 
     meta = TypesParser()
@@ -141,20 +125,23 @@ def test_types_empty():
 
 
 def test_attribute_one_layer():
-    output_data = [[
-		"name",
-	    "height",
-	    "mass",
-	    "hair_color",
-	    "skin_color",
-	    "eye_color",
-	    "birth_year",
-	    "gender",
-	    "species",
-    	"created",
-	    "edited",
-    	"url" 
-        ], []]
+    output_data = [
+        [
+            "name",
+            "height",
+            "mass",
+            "hair_color",
+            "skin_color",
+            "eye_color",
+            "birth_year",
+            "gender",
+            "species",
+            "created",
+            "edited",
+            "url",
+        ],
+        [],
+    ]
 
     meta = AttributesParser()
     meta_attr = meta.attribute_parser(EXAMPLE_2)
@@ -163,24 +150,24 @@ def test_attribute_one_layer():
 
 def test_attributes_multiple_layers():
     output_data = [
-            [
-                "layer1_a",
-                "layer1_b",
-                "layer1_c",
-                "layer1_d",
-                "layer1_e",
-                ],
-            [
-                'layer2_a',
-                'layer2_b',
-                'layer2_c',
-                'layer2_d',
-                'layer3_a',
-                'layer3_b',
-                'layer3_c',
-                'layer4_a'
-                ]
-            ]
+        [
+            "layer1_a",
+            "layer1_b",
+            "layer1_c",
+            "layer1_d",
+            "layer1_e",
+        ],
+        [
+            "layer2_a",
+            "layer2_b",
+            "layer2_c",
+            "layer2_d",
+            "layer3_a",
+            "layer3_b",
+            "layer3_c",
+            "layer4_a",
+        ],
+    ]
 
     meta = AttributesParser()
     meta_attr = meta.attribute_parser(EXAMPLE_3)
@@ -194,17 +181,19 @@ def test_attribute_empty():
 
 
 def test_layer_parser():
-    output_data = [ 
-            ["layer1_a", []],
-            ["layer1_b", []],
-            ["layer1_c",  [["layer2_a", []], ["layer2_b", []]]],
-            ["layer1_d", []],
-            ["layer1_e", [
+    output_data = [
+        ["layer1_a", []],
+        ["layer1_b", []],
+        ["layer1_c", [["layer2_a", []], ["layer2_b", []]]],
+        ["layer1_d", []],
+        [
+            "layer1_e",
+            [
                 ["layer2_c", [["layer3_a", []], ["layer3_b", []]]],
-                ["layer2_d", [["layer3_c", [["layer4_a", []]]]]]
-                ]
-            ]
-        ]
+                ["layer2_d", [["layer3_c", [["layer4_a", []]]]]],
+            ],
+        ],
+    ]
 
     meta = LayersParser()
     meta_layers = meta.layer_parser(EXAMPLE_3)
@@ -218,26 +207,28 @@ def test_layer_parser_empty():
 
 
 def test_layer_processing():
-    input_data = [ 
-            ["layer1_a", []],
-            ["layer1_b", []],
-            ["layer1_c",  [["layer2_a", []], ["layer2_b", []]]],
-            ["layer1_d", []],
-            ["layer1_e", [
+    input_data = [
+        ["layer1_a", []],
+        ["layer1_b", []],
+        ["layer1_c", [["layer2_a", []], ["layer2_b", []]]],
+        ["layer1_d", []],
+        [
+            "layer1_e",
+            [
                 ["layer2_c", [["layer3_a", []], ["layer3_b", []]]],
-                ["layer2_d", [["layer3_c", [["layer4_a", []]]]]]
-                ]
+                ["layer2_d", [["layer3_c", [["layer4_a", []]]]]],
             ],
-            []
-        ]
-    
+        ],
+        [],
+    ]
+
     output_data = [
-    		["layer1_a", "layer1_b", "layer1_c", "layer1_d", "layer1_e"],
-    		["layer2_a", "layer2_b", "layer2_c", "layer2_d"],
-    		["layer3_a", "layer3_b", "layer3_c"],
-    		["layer4_a"]
-    		]
-             
+        ["layer1_a", "layer1_b", "layer1_c", "layer1_d", "layer1_e"],
+        ["layer2_a", "layer2_b", "layer2_c", "layer2_d"],
+        ["layer3_a", "layer3_b", "layer3_c"],
+        ["layer4_a"],
+    ]
+
     meta = LayersParser()
     meta_layers = meta.layer_processing(input_data)
     assert meta_layers == output_data
@@ -250,15 +241,14 @@ def test_layer_processing_empty():
 
 
 def test_layers_retrieval():
-	input_data = [["layer_0"], ["layer_1"], ["layer_2"], ["layer_3"]] 
-	output_data = {
-			"layer_0": ["layer_0"],
-			"layer_1": ["layer_1"],
-			"layer_2": ["layer_2"],
-			"layer_3": ["layer_3"]
-			} 
+    input_data = [["layer_0"], ["layer_1"], ["layer_2"], ["layer_3"]]
+    output_data = {
+        "layer_0": ["layer_0"],
+        "layer_1": ["layer_1"],
+        "layer_2": ["layer_2"],
+        "layer_3": ["layer_3"],
+    }
 
-	meta = LayersParser()
-	meta_layers = meta.layers_retrieval(input_data)
-	assert meta_layers == output_data
-
+    meta = LayersParser()
+    meta_layers = meta.layers_retrieval(input_data)
+    assert meta_layers == output_data
